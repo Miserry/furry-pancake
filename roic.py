539c5dd7a556852ad5c2458b2b873dc5002204e9
Hristo Lakov <107894257+Miserry@users.noreply.github.com>
2022-07-23 23:09:17 +0300

    Update roic.py

--- roic.py
+++ roic.py
@@ -1,13 +1,9 @@
 import requests
 import creds
 
-# oi = int(input("Operating income: "))
-# nta = int(input("Net tangible assets: "))
+company = input("Give me your ticker: ").upper()
+years = int(input("How many years: "))
 
-def get_roic(ebit, net_tangibles):
-    roic = ebit/net_tangibles*100
-    return f'{roic:.2f}%'
-#print(get_roic(oi, nta))
 
 """
 ar - Accounts Receivable
@@ -18,23 +14,17 @@ def_rev = Deferred Revenue
 ocL - Other Current Liabilities
 """
 
-def calculate_nwc(ar, inv, ocA, ap, def_rev, ocL):
-    nwc = ar + inv + ocA - ap - def_rev - ocL
-    if nwc < 0:
-        return 0
-    else:
-        return nwc
-
 def get_nwc(ticker, years):
     balance_sheet = requests.get(
         f"https://financialmodelingprep.com/api/v3/balance-sheet-statement/{ticker}?limit={years}&apikey={creds.api_key}"
     )
+    balance_sheet = balance_sheet.json()
     ar = list(
         reversed(
             [balance_sheet[i]["netReceivables"] for i in range(len(balance_sheet))]
         )
     )
-    inventory = list(
+    inv = list(
         reversed([balance_sheet[i]["inventory"] for i in range(len(balance_sheet))])
     )
     ocA = list(
@@ -60,6 +50,43 @@ def get_nwc(ticker, years):
             ]
         )
     )
-    return ar, inventory, ocA, ap, def_rev, ocL
+    nwc = []
+    for j in range(years):
+        if ar[j] + inv[j] + ocA[j] - ap[j] - def_rev[j] - ocL[j] < 0:
+            nwc.append(0)
+        else:
+            nwc.append(ar[j] + inv[j] + ocA[j] - ap[j] - def_rev[j] - ocL[j])
 
-print(get_nwc("AZO", 5))
+    ppie = list(
+        reversed(
+            [
+                balance_sheet[i]["propertyPlantEquipmentNet"]
+                for i in range(len(balance_sheet))
+            ]
+        )
+    )
+    net_tang = []
+    for i in range(years):
+        net_tang.append(nwc[i] + ppie[i])
+    return net_tang
+
+net_tangible_assets = get_nwc(company, years)
+
+
+def get_ebit(ticker, years):
+    income_statement = requests.get(
+        f"https://financialmodelingprep.com/api/v3/income-statement/{ticker}?limit={years}&apikey={creds.api_key}"
+    )
+    income_statement = income_statement.json()
+    return list(reversed([income_statement[i]["operatingIncome"] for i in range(len(income_statement))]))
+
+ebit = get_ebit(company, years)
+
+roic = []
+for i in range(years):
+    roic.append((ebit[i]/net_tangible_assets[i])*100)
+roic = ['{:.2f}%'.format(elem) for elem in roic]
+net_tangible_assets = ['{:,}'.format(elem) for elem in net_tangible_assets]
+print(f'Return on invested capital: {roic}')
+#print(ebit)
+print(f'Net Tangible Assets: {net_tangible_assets}')
